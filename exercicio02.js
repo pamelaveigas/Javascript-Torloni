0363af69fc38fca4dc1571a33d9d5c9f1ede438b
PAMELA CRISTINA DOS SANTOS VEIGAS <50652492886@imaccommand036.local>
2025-11-19 11:52:42 -0300

    exercicio02 finalizado

---
+++ exercicio02.js
@@ -0,0 +1,34 @@
+let semana = prompt("Digite um número de 1 a 7: ")
+
+switch (semana) {
+    case "1":
+        console.log("Segunda-feira")
+        break;
+    
+        case "2":
+        console.log("Terça-feira")
+        break;
+
+        case "3":
+        console.log("Quarta-feira")
+        break;
+
+        case "4":
+        console.log("Quinta-feira")
+        break;
+
+        case "5":
+        console.log("Sexta-feira")
+        break;
+
+        case "6":
+        console.log("Sabádo")
+        break;
+
+        case "7":
+        console.log("Domingo")
+        break;
+
+    default:
+        break;
+}
