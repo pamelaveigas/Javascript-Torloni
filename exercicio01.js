196b2b42466bfa8164ba24be5e923c6de2a77ce6
PAMELA CRISTINA DOS SANTOS VEIGAS <50652492886@imaccommand036.local>
2025-11-19 11:29:06 -0300

    exercicio01 finalizado

--- exercicio01.js
+++ exercicio01.js
@@ -1,2 +1,27 @@
-window.confirm("Qual seu nome?")
+// let nome = window.prompt("Qual seu nome?")
 
+// let senha = window.prompt("Qual é a senha?")
+
+let login = prompt("Qual seu nome? ")
+
+switch (login) {
+    case "Admin":
+        console.log("Nome correto ")
+        break;
+
+    default:
+        console.log("Nome incorreto! ")
+        break;
+}
+
+let senha = prompt("Digite sua senha: ")
+
+switch (senha) {
+    case "senha123":
+        console.log("Senha correta")
+        break;
+
+    default:
+        console.log("Senha incorreta! ")
+        break;
+}
