6864740656a721e061e420bd7749ebb844dfaffc
PAMELA CRISTINA DOS SANTOS VEIGAS <50652492886@imaccommand036.local>
2025-11-26 16:49:07 -0300

    Iniciado os exercicios iniciais sobre o for

---
+++ Estrutura de repetição/exercicio02.js
@@ -0,0 +1,12 @@
+// 2. Imprimir números pares de 1 a 10.
+
+for (let i = 1; i <= 10; i++) {
+    if (i % 2 == 0) {
+        // par
+        console.log(i)
+    }
+}
+
+for (let i = 2; i <= 10; i = i + 2) {
+    console.log(i);
+}
