0640629e90353e50dfab0651cd9729ccb0153321
PAMELA CRISTINA DOS SANTOS VEIGAS <50652492886@imaccommand036.local>
2025-11-26 08:58:39 -0300

    Finalizando exercicio07

--- exercicio07.js
+++ exercicio07.js
@@ -14,10 +14,19 @@ let valorcompra = parseFloat(prompt("Qual o valor total da compra?"))
 
 let desconto = 0
 
-// if (valorcompra <= 100) {
-//     desconto = 0;
-// } else if (valorcompra <= 200) {
-//     desconto = valorcompra * 0.10;
-// } else {
-//     desconto = valorcompra * 0.20;
-// }
+if (valorcompra <= 100) {
+    desconto = 0;
+} else if (valorcompra <= 200) {
+    desconto = valorcompra * 0.10;
+} else {
+    desconto = valorcompra * 0.20;
+}
+
+let valorFinal = valorcompra - desconto;
+
+alert("Valor da compra: R$ " + valorcompra)
+
+alert("Desconto aplicado: R$ " + desconto)
+
+alert("Valor final a pagar: R$ " + valorFinal)
+
