d36fb64c2cf42f14a2209900c12d88688a67ddfc
PAMELA CRISTINA DOS SANTOS VEIGAS <50652492886@imaccommand036.local>
2025-11-26 14:27:03 -0300

    exercicio08 finalizado

--- exercicio08.js
+++ exercicio08.js
@@ -10,3 +10,29 @@
 // IMC de 35.0 a 39.9: Obesidade grau 2
 // IMC maior ou igual a 40.0: Obesidade grau 3
 
+window.alert("Diga seu peso em quilogramas e sua altura em metros")
+
+let peso = parseFloat(prompt("Digite seu peso em quilograma: "));
+
+let altura = parseFloat(prompt("Digite sua altura em metros: "));
+
+let IMC = (peso / (altura * altura));
+
+if (IMC <= 18.5) {
+    alert("Abaixo do peso")
+}
+else if (IMC >= 18.5 && IMC <= 24.9) {
+    alert("Peso normal")
+}
+else if (IMC >= 25.0 && IMC <= 29.9) {
+    alert("Sobrepeso")
+}
+else if (IMC >= 30.0 && IMC <= 34.9) {
+    alert("Obesidade grau 1")
+}
+else if (IMC >= 35.0 && IMC <= 39.9) {
+    alert("Obesidade grau 2")
+}
+else if (IMC >= 40.0) {
+    alert("Obesidade grau 3. Se cuide por favor, sua vida está em risco!")
+}
