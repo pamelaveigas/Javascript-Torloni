cbffba9344e47b9f4d15b3ff05a8baaf5bb607f1
PAMELA CRISTINA DOS SANTOS VEIGAS <50652492886@imaccommand036.local>
2025-11-28 11:26:31 -0300

    whileexer08 finalizado

---
+++ Estrutura de repetição/whileexer09.js
@@ -0,0 +1,8 @@
+//Faça um programa que receba um número do usuário e continue
+// a pedir o número se o numero for diferente de 0.
+
+let numeroescolhido = prompt("Digite um número: ");
+
+while (numeroescolhido != 0) {
+    numeroescolhido = prompt("Por favor, digite novamente um número: ")
+}
