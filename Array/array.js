4f2dc534d7707799eca024d8f3e3431a60946a1e
PAMELA CRISTINA DOS SANTOS VEIGAS <50652492886@imaccommand036.local>
2025-11-28 13:38:28 -0300

    array finalizado

--- Array/array.js
+++ Array/array.js
@@ -1,1 +1,36 @@
-let frutas
+// let frutas = ["Goiaba", "Morango", "Melancia", "Uva", "Pera"]; //--array/lista vazia--
+
+// //length igual a comprimento, ou seja, a quantidade de itens que q lista tem.--
+// console.log("A lista de frutas tem " + frutas.length + " itens.");
+
+// console.log("A primeira fruta é a: " + frutas[0]);
+
+// // acesse o item 4 da lista e exiba-o no console--
+// console.log("A quarta fruta é a: " + frutas[3]);
+
+// //exibe todas as frutas do array e com o seu formato--
+// console.log(frutas);
+
+// for (let i = 0; i <= frutas.length; i++) {
+//     console.log(frutas[i]);
+// }
+
+// //Crie uma lista de tarefas, contendo no max 10 itens e exiba cada tarefas--
+
+let tarefas = ["Ir na feira", "Comprar presente", "Buscar o filho na escola", "Reunião", "Fazer janta"];
+
+
+//length igual a comprimento, ou seja, a quantidade de itens que a lista tem.
+console.log("A lista de tarefas tem " + tarefas.length + " itens.");
+
+console.log("A primeira tarefa é: " + tarefas[0]);
+
+// acesse o item 4 da lista e exiba-o no console
+console.log("A quarta tarefa é: " + tarefas[3]);
+
+//exibe todas as tarefas do array e com o seu formato
+console.log(tarefas);
+
+for (let i = 0; i <= tarefas.length; i++) {
+    console.log(tarefas[i]);
+}
