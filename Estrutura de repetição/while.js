49813ceda6969423504f2830d0addbb5fa2d42d7
PAMELA CRISTINA DOS SANTOS VEIGAS <50652492886@imaccommand036.local>
2025-11-28 10:09:46 -0300

    Finalizado while

---
+++ Estrutura de repetição/while.js
@@ -0,0 +1,9 @@
+let volta = 0; //inicialização
+
+let opcaoescolhida = parseInt(prompt("Digite 1 para continuar: "));
+
+//condição
+while (opcaoescolhida !== 1) {
+    opcaoescolhida = parseInt(prompt("Voce precisa escolher o número 1 para continuar"))
+
+}
