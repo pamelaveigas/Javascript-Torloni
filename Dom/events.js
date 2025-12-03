7497a66165e668add25ad5079093e691a772196e
PAMELA CRISTINA DOS SANTOS VEIGAS <50652492886@imaccommand036.local>
2025-12-03 13:39:25 -0300

    Atividade events.js finalizada

--- Dom/events.js
+++ Dom/events.js
@@ -3,12 +3,14 @@
 
 let elementoBtn = document.getElementById("btn")
 let novoElemento = document.createElement("a");
-novoElemento.textContent = "Link";
+novoElemento.textContent = "Luan Santana - Registro Histórico"
+novoElemento.href = "https://www.youtube.com/watch?v=ZYd2yeeEad4&list=RDZYd2yeeEad4&start_radio=1";
 
 elementoBtn.addEventListener("click", () => {
     //alert("Você clicou no botão");
 
     //elementoBtn.style.backgroundColor = "purple"; (trocar a cor do botao)
 
+    document.body.appendChild(novoElemento);
 })
 
