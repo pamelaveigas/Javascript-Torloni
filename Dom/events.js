a5adaf1feef3635652180440f76495bed878004d
PAMELA CRISTINA DOS SANTOS VEIGAS <50652492886@imaccommand036.local>
2025-12-03 14:06:54 -0300

    finalizado events

--- Dom/events.js
+++ Dom/events.js
@@ -1,9 +1,10 @@
 //eventos sao interacoes do usuario com o websit e também
 //comportamentos do website, por exemplo carregara tala
 
-let elementoBtn = document.getElementById("btn")
+let elementoBtn = document.getElementById("btn");
+let elementoIdLeandra = document.getElementById("tecnica");
 let novoElemento = document.createElement("a");
-novoElemento.textContent = "Luan Santana - Registro Histórico"
+novoElemento.textContent = "Luan Santana - Registro Histórico";
 novoElemento.href = "https://www.youtube.com/watch?v=ZYd2yeeEad4&list=RDZYd2yeeEad4&start_radio=1";
 
 elementoBtn.addEventListener("click", () => {
@@ -12,5 +13,8 @@ elementoBtn.addEventListener("click", () => {
     //elementoBtn.style.backgroundColor = "purple"; (trocar a cor do botao)
 
     document.body.appendChild(novoElemento);
+    //document.body.removeChild(elementoIdLeandra);
+    //console.log(document.body.children);
+    document.body.children[2].removeChild(elementoIdLeandra)
 })
 
